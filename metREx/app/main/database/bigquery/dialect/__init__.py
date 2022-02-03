327d2885dca44e111215bcbce6126ce004446aaf
Phillipson, Michael <MICHAEL_PHILLIPSON1@homedepot.com>
2022-02-03 14:32:29 -0600

    Suppressed SQLAlchemy statement cache warning for BigQuery dialect

--- metREx/app/main/database/bigquery/dialect/__init__.py
+++ metREx/app/main/database/bigquery/dialect/__init__.py
@@ -4,6 +4,8 @@ from pybigquery.sqlalchemy_bigquery import BigQueryDialect as BaseDialect
 
 
 class BigQueryDialect(BaseDialect):
+    supports_statement_cache = False
+
     def create_connect_args(self, url):
         query = url.query
 
